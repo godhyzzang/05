d7e0b2548410ba54988cd829f482542b4004bd0e
godhyzzang <amksh0914@gmail.com>
2023-10-05 16:28:37 +0900

    4. 덧셈 프로그램

--- main.c
+++ main.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
 int main() {
+	//4. 덧셈 프로그램
+	//입력한 숫자를 1부터 해당 숫자까지 모두 더하는 계산
+	int number;
+	int sum=0; //sum 0 초기화해야함
 	
-	int inputnumber;
-	scanf_s("%i", &inputnumber);
+	printf("input a number");
+	scanf_s("%i", &number);
 
-	if (inputnumber < 0)
-		printf("%i", -inputnumber);
-	else
-		printf("%i", inputnumber);
+	for (int i = 0; i < number; i++) {
+		
+		
+		sum += (i+1);
+	}
+	
+	printf("Total : %i",sum);
 
 }
