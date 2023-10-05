a9346df8156df100271c0c74245516f348c45063
godhyzzang <amksh0914@gmail.com>
2023-10-05 17:29:00 +0900

    6. 숫자 맞추기 게임

--- main.c
+++ main.c
@@ -1,20 +1,41 @@
 #include <stdio.h>
 
 int main() {
-	//3
-	
-	char c;
-	int num=0;
-	scanf_s("%c", &c);
-	
+	//6
 
-	while ((c=getchar())!='\n') {
-		if('0'<=c && c<='9'){
-			num += 1; 
+	int correct_num = 6;
+	int input_num;
+	int trial = 0;
+
+
+	while (1) {
+		//true 빌드 왜 안되지..? 식별자 True정의되어있지 않다고 나옴 
+		trial += 1;
+		printf("input a number\n");
+		scanf_s("%i", &input_num);
+
+		if (input_num < correct_num) {
+			printf("please input BIGGER number\n\n");
+			/*trial += 1;*/
+			/*continue;*/
 		}
-	}
-	printf("number of int : %i",num);
 
-	
+		else if (input_num > correct_num) {
+			printf("please input Smaller number\n\n");
+			/*trial += 1;*/
+			/*continue;*/
+		}
+
+		else {
+			printf("correct\n");
+			printf("Your trial : %i", trial);
+			break;
+		}
+
+
+		//break;
+	}
+
+
 
 }
